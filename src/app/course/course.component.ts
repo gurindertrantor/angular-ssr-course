d729c0f45c08192afc98975d5f94c54abf736a8c
Gurinderpal Singh <gurinderpal.singh@trantorinc.com>
2024-09-04 11:32:26 +0530

    reset to starting code

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -30,34 +30,24 @@ export class CourseComponent implements OnInit {
     displayedColumns= ["seqNo", "description", "duration"];
 
 
-  constructor(
-    private route: ActivatedRoute,
-    private coursesService: CoursesService,
-    private title: Title,
-    private meta: Meta) {}
+    constructor(
+        private route: ActivatedRoute,
+        private coursesService: CoursesService) {
 
-  ngOnInit() {
-
-    this.course = this.route.snapshot.data["course"];
-
-    this.title.setTitle(this.course.description);
-
-    this.meta.updateTag({name: "description", content: this.course.longDescription});
-
-    this.meta.addTag({name: 'twitter:card', content: 'summary'});
-    this.meta.addTag({name: 'twitter:site', content: '@AngularUniv'});
-    this.meta.addTag({name: 'twitter:title', content: this.course.description});
-    this.meta.addTag({name: 'twitter:description', content: this.course.description});
-    this.meta.addTag({name: 'twitter:text:description', content: this.course.description});
-    this.meta.addTag({name: 'twitter:image', content: 'https://avatars3.githubusercontent.com/u/16628445?v=3&s=200'});
+    }
 
 
-    this.dataSource = new MatTableDataSource([]);
 
-    this.coursesService.findAllCourseLessons(this.course.id)
-      .subscribe(lessons => this.dataSource.data = lessons);
+    ngOnInit() {
 
-  }
+        this.course = this.route.snapshot.data["course"];
+
+        this.dataSource = new MatTableDataSource([]);
+
+        this.coursesService.findAllCourseLessons(this.course.id)
+            .subscribe(lessons => this.dataSource.data = lessons);
+
+    }
 
 
 }
